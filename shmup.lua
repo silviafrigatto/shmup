057cde7eb35c61931117015275524d4ba8498a74
Silvia Frigatto <silvia.frigatto@gmail.com>
2026-04-01 16:25:14 -0300

    Moving the ship / player

--- shmup.lua
+++ shmup.lua
@@ -1,12 +1,28 @@
 function _init()
-    
+    cls()
+    --Ship (player) variables
+    ship_sprite=1
+    ship_speed=2
+    ship_x=64
+    ship_y=64
 end
 
 function _update()
-    
+    --Move the ship (left, right, up and down)
+    if btn(0) then
+        ship_x=ship_x-ship_speed
+    elseif btn(1) then
+        ship_x=ship_x+ship_speed
+    elseif btn(2) then
+        ship_y=ship_y-ship_speed
+    elseif btn(3) then
+        ship_y=ship_y+ship_speed
+    end    
 end
 
 function _draw()
+    --Draw the ship
     cls()
-    spr(01,64,64)
+    spr(ship_sprite,ship_x,ship_y)
 end
+
